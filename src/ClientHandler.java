422f46f23ed7615c9eda7a1ee310d582dab028d7
HansenRasmus-ux <rasmus.ankerhansen@gmail.com>
2025-03-27 16:35:20 +0100

    Works now!
    Needs more tweaking and GUI

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -15,9 +15,9 @@ public class ClientHandler implements Runnable {
             this.socket = socket;
             this.out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())); //Bruges til at sende beskeder ud ting andre.
             this.in = new BufferedReader(new InputStreamReader(socket.getInputStream())); //Bruges til at modtage og læse beskeder.
-            this.username = this.in.readLine(); // Displayer personens brugernavn, før personens besked.
+            this.username = this.in.readLine(); // Displayer personens brugernavn, før personens besked. // venter på at beskeden bliver sendt ud, før den reagere.
             clientHandlers.add(this); // Tilfører brugerene til gruppen.
-            broadcastMessage("Server: " + username + "er deltaget i gruppen!");
+            broadcastMessage("Server: " + username + " er deltaget i gruppen!");
         } catch (IOException e){
             closeEverything(socket, in, out);
         }
--- src/Clients.java
+++ src/Clients.java
@@ -1,4 +1,87 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
 public class Clients {
-    public static void main(String[] args) {
+    private Socket socket;
+    private BufferedReader in;
+    private BufferedWriter out;
+    private String username;
+
+    public Clients(Socket socket, String username) {
+        try{
+            this.socket = socket;
+            this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.username = username;
+        } catch (IOException e) {
+            closeEverything(socket,in,out);
+        }
+    }
+
+    public void send() {
+        try {
+            out.write(username);
+            out.newLine();
+            out.flush();
+
+            Scanner scanner = new Scanner(System.in);
+            while(socket.isConnected()) {
+                String msg = scanner.nextLine();
+                out.write(username + ": " + msg);
+                out.newLine();
+                out.flush();
+            }
+        } catch (IOException e) {
+            closeEverything(socket,in,out);
+        }
+    }
+
+    public void receive() {
+        new Thread(new Runnable() {
+
+            String msgfrommember;
+
+            @Override
+            public void run() {
+                while(socket.isConnected()) {
+                    try {
+                        msgfrommember = in.readLine();
+                        System.out.println(msgfrommember);
+                    } catch (IOException e) {
+                        closeEverything(socket,in,out);
+                    }
+                }
+
+            }
+        }).start();
+
+    }
+
+    public void closeEverything(Socket socket, BufferedReader in, BufferedWriter out) {
+        try {
+            if (in != null){
+                in.close();
+            }
+            if (out != null){
+                out.close();
+            }
+            if (socket != null){
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("indtast dit brugernavn: ");
+        String username = scanner.nextLine(); //Få fat i brugerens brugernavn
+        Socket socket = new Socket("localhost", 1234);
+
+        Clients clients = new Clients(socket,username);
+        clients.receive();
+        clients.send();
     }
 }
